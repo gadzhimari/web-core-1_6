519d447da47309fa37ab1607718052533dba00de
karicarlos <kadimowa@gmail.com>
2025-11-14 18:47:39 +0300

    add swiper.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,32 @@
 import './sass/main.scss';
+
+var init = false;
+var swiper;
+
+if (window.innerWidth < 784) {
+    if (!init) {
+        init = true;
+        swiper = new Swiper('.swiper', {
+            direction: 'horizontal',
+            loop: true,
+            slidesPerView: "auto", 
+            spaceBetween: 0,
+            slideToClickedSlides: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            grabCursor: true,
+            keyboard: {
+                enabled: true,
+                onlyInViewport: true,
+                pageUpDown: true,
+            },
+        });
+    }
+    } else if (init) { 
+    if (swiper) {
+        swiper.destroy();
+    }
+    init = false;
+}
