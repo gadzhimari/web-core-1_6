a39f98bb37d609667ae518caea9d27dfea819d68
Gadzhimari Kachaev <gadzhimari@gmail.com>
2025-11-28 18:49:43 +0300

    change vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,26 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
-export default defineConfig({
-  publicDir: 'public',
-  base: '/web-core-1_6/',
-  build: {
-    outDir: 'dist',
-    cssCodeSplit: true,
-    emptyOutDir: true,
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: 'index.html', 
+export default defineConfig(({ mode }) => {
+  const base = process.env.NODE_ENV === "production" ? "/" : "/web-core-1_6/";
+
+  return {
+    publicDir: "public",
+    base,
+    build: {
+      outDir: "dist",
+      cssCodeSplit: true,
+      emptyOutDir: true,
+      assetsDir: ".",
+      rollupOptions: {
+        input: {
+          main: "index.html",
+        },
       },
     },
-  },
 
-  server: {
-    port: 3000,
-    open: true,
-  },
+    server: {
+      port: 3000,
+      open: true,
+    },
+  };
 });
